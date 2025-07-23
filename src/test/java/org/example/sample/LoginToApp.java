5873886e20b432c2c716303e488b2f716ea32dbc
navaneethhhk <hknavaneeth@gmail.com>
2025-07-23 08:27:25 +0530

    2307

--- src/test/java/org/example/sample/LoginToApp.java
+++ src/test/java/org/example/sample/LoginToApp.java
@@ -1,4 +1,15 @@
 package org.example.sample;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
 public class LoginToApp {
+    @Test
+    public void test(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com/");
+
+
+    }
 }
